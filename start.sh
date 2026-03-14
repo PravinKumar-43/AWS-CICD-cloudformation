c421de6a71cdb3425891dd12677889c378d3d5f1
PravinKumar-43 <pravinkumarlava143@gmail.com>
2026-03-14 09:10:23 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker stop calculator || true
+docker rm calculator || true
+
+docker build -t calculator-application .
+
+docker run -d -p 5000:5000 --name calculator calculator-application
