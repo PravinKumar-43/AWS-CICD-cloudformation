e1d24e5d8682fc9fe067d3a2ab9734f291cef883
PravinKumar-43 <pravinkumarlava143@gmail.com>
2026-03-14 10:06:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,24 +8,26 @@ def home():
     <h2>Simple Calculator</h2>
 
     <h3>Add and Subtract</h3>
-    <form action="/add">
-    <form action="/sub">
+    <form action="/calculate">
         Number 1: <input name="a"><br>
-        Number 2: <input name="b"><br>
-        <input type="submit" value="Add">
-        <input type="submit" value="Subtract">
+        Number 2: <input name="b"><br><br>
+
+        <button type="submit" name="op" value="add">Add</button>
+        <button type="submit" name="op" value="sub">Subtract</button>
     </form>
+    '''
 
-@app.route('/add')
-def add():
+@app.route('/calculate')
+def calculate():
     a = int(request.args.get('a'))
     b = int(request.args.get('b'))
-    return f"<h3>Result: {a + b}</h3>"
+    op = request.args.get('op')
 
-@app.route('/sub')
-def sub():
-    a = int(request.args.get('a'))
-    b = int(request.args.get('b'))
-    return f"<h3>Result: {a - b}</h3>"
+    if op == "add":
+        result = a + b
+    elif op == "sub":
+        result = a - b
+
+    return f"<h3>Result: {result}</h3>"
 
 app.run(host="0.0.0.0", port=5000)
