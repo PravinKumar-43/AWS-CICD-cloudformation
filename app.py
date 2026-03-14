590347341421432d76fe5a553894dd3a7f69ec98
PravinKumar-43 <pravinkumarlava143@gmail.com>
2026-03-14 08:30:07 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '''
+    <h2>Simple Calculator</h2>
+    <form action="/add">
+        Number 1: <input name="a"><br>
+        Number 2: <input name="b"><br>
+        <input type="submit" value="Add">
+    </form>
+    '''
+
+@app.route('/add')
+def add():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    return f"<h3>Result: {a+b}</h3>"
+
+app.run(host="0.0.0.0", port=5000)
